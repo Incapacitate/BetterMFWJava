6cc8087dc49a0f25960c31a5ef898d1bb83b887b
Incapacitate <46305266+Incapacitate@users.noreply.github.com>
2019-01-04 23:39:15 +0100

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+let xxxvidsxxx = 'viens voir notre site !'
+
+function alert() {
+		if ( confirm(xxxvidsxxx) ) {
+    window.location.assign("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+}
+else {
+    
+}
+}
+
+setTimeout(alert, 10000);
